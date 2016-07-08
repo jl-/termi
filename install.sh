c48246af80d0748e1b3dabcad99747723bce4916
zhengjianglong <jl@zhengjianglongs-MacBook-Pro.local>
2016-07-08 10:16:30 +0800

    utils in install

--- install.sh
+++ install.sh
@@ -1,8 +1,46 @@
 #! /bin/bash
 set -e
 
-# import utils
-. "$(dirname $0)/utils.sh"
+################################## utils
+# . "$(dirname $0)/utils.sh"
+# @param: target, item1, ..., itemn
+function is_in_array() {
+  local target=$1; shift
+  local item
+  for item; do
+    [ $item == $target ] && return 0
+  done
+  return 1
+}
+
+# @param: question, question_warning, allowed_answer1, ...allowed_answern
+function ask_question() {
+  local readonly question=$1; shift
+  local readonly question_warning=${1:-$question}; shift
+  local readonly allowed_answers=${@}
+  local answer
+
+  printf '%b ' 1>&2 $question
+  read answer
+  while ! $(is_in_array $answer $allowed_answers); do
+    printf '%b ' 1>&2 $question_warning
+    read answer
+  done
+
+  echo $answer
+}
+
+# @param: question, question_warning
+function confirm() {
+  local readonly question="$1 [Y/N]"
+  local readonly question_warning="${2:-$1} [Y/N]"
+  local readonly answer=$(ask_question "$question" "$question_warning" Y N)
+  [ $answer == 'Y' ] && return 0 || return 1
+}
+###################################### end: utils
+
+
+
 
 # Installation path defaults to `$HOME/.termi`
 # You may custom this by setting `TERMI_PATH`:
@@ -144,11 +182,9 @@ function with_vim() {
 }
 
 function with_nvim() {
-  if [ -d ${HOME}/.config ]; then
-    mkdir -p ${HOME}/.config/nvim
-    ln -sf ${TERMI_PATH}/vim/.vim ${HOME}/.config/nvim
-    ln -sf ${TERMI_PATH}/vim/.vimrc ${HOME}/.config/nvim/init.vim
-  fi
+  mkdir -p ${HOME}/.config/nvim
+  ln -sf ${TERMI_PATH}/vim/.vim ${HOME}/.config/nvim
+  ln -sf ${TERMI_PATH}/vim/.vimrc ${HOME}/.config/nvim/init.vim
 }
 
 function with_tmux() {
