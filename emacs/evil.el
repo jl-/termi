ce3d497e722a195f3ad9b1a65ddc57c4ddf02c6e
z.cius <zr.cius@gmail.com>
2018-11-02 15:41:40 +0800

    ingnore runtime files

--- emacs/evil.el
+++ emacs/evil.el
@@ -1,6 +1,14 @@
 ;; Evil mode
+;; C-z to toggle
 (use-package evil
              :ensure t
              :config
              (evil-mode 1))
+
+(use-package evil-escape
+             :ensure t
+             :init
+             (setq-default evil-escape-key-sequence "jk")
+             :config
+             (evil-escape-mode 1))
 
